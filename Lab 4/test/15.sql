52ae6577183391c94b350da80ed06375664f5532
DaoistDuck <dragonoath123@gmail.com>
2021-09-23 21:26:12 -0700

    got 15 to work

--- Lab 4/test/15.sql
+++ Lab 4/test/15.sql
@@ -1,5 +1,7 @@
 --SQLite 
 
-select count(*)
-from 
-where 
+select count(distinct o_orderkey)
+from customer, supplier, lineitem, orders
+where c_acctbal > 0 AND s_acctbal < 0
+AND c_custkey = o_custkey AND s_suppkey = l_suppkey 
+AND l_orderkey = o_orderkey;
