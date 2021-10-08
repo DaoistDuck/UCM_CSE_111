45d47d372ee1775cfca5f087bee5c06c90b5c4b7
DaoistDuck <dragonoath123@gmail.com>
2021-10-08 15:36:30 -0700

    lost

--- Lab 6/test/8.sql
+++ Lab 6/test/8.sql
@@ -3,29 +3,20 @@
 --WITH justAmerican AS
 --(
 
+    --SELECT s_suppkey FROM supplier
+
     
 --)
 --SELECT count(*)
-WITH tmp AS(
-SELECT s_suppkey
-FROM region
-INNER JOIN nation on n_nationkey = r_nationkey
-INNER JOIN supplier on s_nationkey = n_nationkey
-WHERE r_name in ('AMERICA')
-), anotherTmp AS(
-    SELECT s_suppkey
-FROM region
-INNER JOIN nation on n_nationkey = r_nationkey
-INNER JOIN supplier on s_nationkey = n_nationkey
-WHERE r_name <> 'AMERICA'
-        
-)
 SELECT DISTINCT c_name
-    FROM supplier, nation, region, customer, orders, lineitem, 
-    WHERE s_nationkey = n_nationkey AND n_regionkey = r_regionkey AND r_name in ('AMERICA')
-    AND s_suppkey = l_suppkey AND l_orderkey = o_orderkey
-    AND c_custkey = o_custkey --AND c_nationkey = s_nationkey
-    GROUP BY c_name
-    HAVING count(l_quantity) >= 1
+FROM supplier, nation, region, customer, orders
+WHERE s_nationkey = n_nationkey AND n_regionkey = r_regionkey 
+AND r_name == 'AMERICA'
+AND c_custkey = o_custkey
+
+--AND s_suppkey = l_suppkey AND l_orderkey = o_orderkey
+--AND c_custkey = o_custkey
+GROUP BY c_name
+HAVING count(o_orderkey) >= 1
 
 
